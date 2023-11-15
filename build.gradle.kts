4c60e44a1fe604659b9f6b40ed582368dad06531
Cam <me@cameronbowe.net>
2023-11-15 17:49:32 -0600

    Fix Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import java.net.URI
+
 group = "net.cameronbowe" //The project's group.
 version = "1.0.0" //The project's version.
 description = "A utility to access Vouchley's API with ease." //The project's description.
@@ -20,6 +22,41 @@ dependencies {
     //Shaded dependencies.
     implementation("com.google.code.gson", "gson", "2.8.8") //Implement GSON (for fantastic JSON usage).
     implementation("org.apache.httpcomponents", "httpclient", "4.5.13") //Implement apache http client (for http requests).
+}
+
+//The publishing to maven.
+publishing {
+
+    //The publishing repository.
+    repositories {
+
+        //Add the Maven Central repository.
+        maven {
+            url = uri("https://s01.oss.sonatype.org/content/repositories/releases/") //The sonatype repository.
+            credentials {
+                username = project.findProperty("ossrhUsername")?.toString()
+                password = project.findProperty("ossrhPassword")?.toString()
+            }
+        }
+
+    }
+
+    //The publishing publications (what to publish).
+    publications {
+
+        //The maven java publication.
+        register("mavenJava", MavenPublication::class) {
+            artifact(tasks.named("shadowJar").get()) { classifier = "all" } //Publish the shadow jar.
+        }
+
+    }
+}
+
+//The signing (of the maven publication).
+signing {
+    sign(publishing.publications["mavenJava"]) //Sign the maven publication.
+    sign(configurations.archives.get()) //Sign the archives.
+    useGpgCmd() //Use the gpg command.
 }
 
 //Compilation.
