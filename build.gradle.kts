0427ac4c29510b41c188764d1a847264f2170d97
Cam <me@cameronbowe.net>
2023-11-15 17:05:34 -0600

    Fix Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import java.net.URI
-
 group = "net.cameronbowe" //The project's group.
 version = "1.0.0" //The project's version.
 description = "A utility to access Vouchley's API with ease." //The project's description.
@@ -24,51 +22,6 @@ dependencies {
     implementation("org.apache.httpcomponents", "httpclient", "4.5.13") //Implement apache http client (for http requests).
 }
 
-//The publishing to maven.
-publishing {
-    repositories {
-        maven {
-            url = URI.create("https://oss.sonatype.org/content/repositories/releases/") //The repository's url.
-            credentials { //The repository's credentials.
-                username = project.findProperty("ossrhUsername")?.toString() //The repository's username.
-                password = project.findProperty("ossrhPassword")?.toString() //The repository's password.
-            }
-        }
-    }
-    publications {
-        register("mavenJava", MavenPublication::class) {
-            artifact(tasks.named("shadowJar").get()) { classifier = "all" } //Publish the shadow jar.
-            pom { //The project's pom.
-                url.set("https://vouchley.com") //The project's url.
-                developers { //The project's developers.
-                    developer { //The developer.
-                        id.set("cameronbowe") //The developer's id.
-                        name.set("Cameron Bowe") //The developer's name.
-                        email.set(project.findProperty("ossrhEmail")?.toString()) //The developer's email.
-                    }
-                }
-                licenses { //The project's licenses.
-                    license {
-                        name.set("CC0-1.0")
-                        url.set("https://creativecommons.org/publicdomain/zero/1.0/legalcode.en")
-                    }
-                }
-                scm { //The project's scm.
-                    url.set("https://github.com/cameronbowe/VouchleyAPI") //The project's url.
-                    connection.set("scm:git:git://github.com/cameronbowe/VouchleyAPI.git") //The project's connection.
-                    developerConnection.set("scm:git:ssh://git@github.com:cameronbowe/VouchleyAPI.git") //The project's developer connection.
-                }
-            }
-        }
-    }
-}
-
-//The signing (of the maven publication).
-signing {
-    sign(publishing.publications["mavenJava"]) //Sign the maven publication.
-    useGpgCmd() //Use the gpg command.
-}
-
 //Compilation.
 tasks {
 
@@ -84,10 +37,4 @@ tasks {
         exclude("**/*.txt", "**./META-INF/*") //Exclude gibberish files.
     }
 
-    //Publish to maven local.
-    named("publishToMavenLocal").configure {
-        doLast {
-            println("Publishing project to local Maven repository...") //Log that we are publishing to maven local.
-        }
-    }
 }
